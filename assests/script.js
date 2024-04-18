2a93a86453240992235c04439a4b0e1450311baa
DanielMutunga <mutungadaniel98@gmail.com>
2024-04-19 01:51:24 +0300

    Made some Changes

---
+++ assests/script.js
@@ -0,0 +1,6 @@
+document.getElementById("new").onclick = function (){
+    window.location.href = "assests/registration.html"
+}
+document.getElementById("login").onclick = function (){
+    window.location.href = "assests/login.html"
+}
